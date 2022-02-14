0da914bbfd6d1a79d432cd5fe3bce832efc42b56
Nadia Lut <3197344@gmail.com>
2022-02-14 14:21:09 +0200

    add hover

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './sass/main.scss';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+// Your API key: 7652668-fcb425495cfb1d754d33171ff
